95c3f0227057f14046c4b696fd5930664e18234d
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-23 12:39:42 -0600

    Basic styling of GitHubUser component using less and ANT design library. added animated skeleton that shows while user data is loading

--- git-status/src/App.js
+++ git-status/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import NavBar from "./components/NavBar/NavBar";
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PrivateRoute from "./authorization/PrivateRoute";
 import Register from "./components/Register/Register";
 import Search from "./components/Search/Search";
