fe26a1f1d755a168e0955551177bf3d3439b8d8a
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-23 16:04:21 -0600

    Styled search results to show a grid of cards

--- git-status/src/components/Search/SearchResult.js
+++ git-status/src/components/Search/SearchResult.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Card } from 'antd';
+const { Meta } = Card;
 
 function SearchResult(props) {
 
     console.log("SearchResult", props.result)
 
     return (
-        <div>
+        <div className="individualSearchResult">
             <Link to={"/user/" + props.result.login}>
-                <img src={props.result.avatar_url} />
-                <p>{props.result.login}</p>
+                {/* <img src={props.result.avatar_url} />
+                <p>{props.result.login}</p> */}
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={props.result.avatar_url} />}
+                >
+                    <Meta title={props.result.login} description="Name will go here" />
+                </Card>
             </Link>
         </div>
     );
